54a8468c9e680f2464dbd451ad98cfdaa46be6fc
Iskander Dino <iskanderdino@yahoo.com>
2020-03-20 04:38:26 +0800

    adding login feature

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,8 @@ Rails.application.routes.draw do
 
   resources :articles
   resources :users, except: [:new]
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 end
